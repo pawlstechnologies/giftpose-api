b72809039b5206b8f7506ab6082016b696469ee9
Oluwatobi Solomon <solotobby@gmail.com>
2026-04-06 12:33:37 +0100

    updated push notification

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,18 @@
 // import admin from "firebase-admin";
 import * as admin from 'firebase-admin';
-import serviceAccount  from "../serviceAccountKey.json";
+import serviceAccount from "../serviceAccountKey.json";
 
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+  })
+
 });
 
 export default admin;
