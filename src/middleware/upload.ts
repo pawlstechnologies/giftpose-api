ad2d9b0b4659035913b24f2df4eace502d4a7619
Oluwatobi Solomon <solotobby@gmail.com>
2026-04-06 09:04:14 +0100

    upload images

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,8 +1,17 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+// 🔥 Ensure uploads folder exists
+const uploadPath = path.join(__dirname, '../../uploads');
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 
 const storage = multer.diskStorage({
-  destination: 'uploads/',
+  destination: uploadPath,
   filename: (_, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
